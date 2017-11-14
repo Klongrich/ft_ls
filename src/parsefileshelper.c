ef842008c59521d2e0a40cb9d59a9ab3062365e5
Kyle LONGRICH <klongric@e1z3r1p16.42.us.org>
2017-11-14 06:32:09 -0800

    adding

--- src/parsefileshelper.c
+++ src/parsefileshelper.c
@@ -13,5 +13,9 @@ int		is_file(const char *path)
 	struct stat path_stat;
 
 	stat(path, &path_stat);
+	if (S_ISCHR(path_stat.st_mode))
+		return (1);
+	if (S_ISBLK(path_stat.st_mode))
+		return (1);
 	return (S_ISREG(path_stat.st_mode));
 }
